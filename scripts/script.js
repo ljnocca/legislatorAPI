77f7bc9ca2dc5a9fa34fc416bd922ea0d761ff5b
Laurent Nocca <ljnocca@gmail.com>
2017-02-20 22:11:00 -0600

    1st draft

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+
+function legislatorToHTML(legislatorObj){
+    var legislatorStr = ''
+    legislatorStr += '<div class="legislator">'
+    legislatorStr +=    '<h1 class="name">' + legislatorObj.first_name + " " + legislatorObj.last_name + '</h1>'
+    legislatorStr +=    '<h2 class="party">' + legislatorObj.title +"--"+ legislatorObj.party + "-"+legislatorObj.state +'</h2>'
+    legislatorStr +=    '<ul class="contactInfo">'
+    legislatorStr +=        '<li> email: ' + legislatorObj.oc_email + '</li>'
+    legislatorStr +=        '<li> website: ' + legislatorObj.website + '</li>'
+    legislatorStr +=        '<li> facebook: ' + legislatorObj.facebook_id + '</li>'
+    legislatorStr +=        '<li> twitter: ' + legislatorObj.twitter_id + '</li>'
+    legislatorStr +=    '</ul>'
+    legislatorStr +=    '<p class = "termEnd"> Term End: ' + legislatorObj.term_end + '</p>'
+    legislatorStr += '</div>'
+
+    return legislatorStr
+}
+
+
+function handleResponse(apiResponse) {
+    var stringHTML = ''
+    var containerNode = document.querySelector('#container')
+
+    for (var i = 0; i<apiResponse.results.length; i++){
+        stringHTML += legislatorToHTML(apiResponse.results[i])
+    }
+    containerNode.innerHTML = stringHTML
+}
+
+var promise = $.getJSON('https://congress.api.sunlightfoundation.com/legislators')
+
+promise.then(handleResponse)
